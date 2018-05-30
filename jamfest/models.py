f3965d342e545cf86633325fbc097d8c8c1785ea
alexkarari <k.alexander@gmail.com>
2018-05-30 12:07:48 +0300

    Models
    initial classes/tables created for jamfest

--- jamfest/models.py
+++ jamfest/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    avatar = models.ImageField(upload_to='profilepic/')
+    bio = models.TextField()
+    price = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Events(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.TextField()
+    poster = models.ImageField(upload_to='poster/')
+    eventtime = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    post = models.TextField()
+    image = models.ImageField(upload_to='newsimages/')
+    postDate = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+
+class Releases(models.Model):
+    album_title = models.CharField(max_length=100)
+    album_image = models.ImageField(upload_to='album/')
+    producer = models.CharField(max_length=100)
+    releaseDate = models.DateTimeField(auto_now_add=True)
+
+class Merchandise(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_description = models.CharField(max_length=100)
+    item_image = models.ImageField(upload_to='merch/')
+    item_price = models.CharField(max_length=100)
