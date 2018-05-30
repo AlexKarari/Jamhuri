9d66e9f2db7f7e4c7f7ab8b54f71cb493312e3a7
alexkarari <k.alexander@gmail.com>
2018-05-30 17:23:48 +0300

    Events
    events details displayed on index page

--- jamfest/views.py
+++ jamfest/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Artist, Events, News, Releases, Merchandise
 
 # Create your views here.
+
+
+def index(request):
+    '''
+    View function that displays the homepage and all its contents 
+    '''
+    events = Events.objects.all()
+    return render(request, 'all/index.html', {"events": events})
+
+def events(request):
+        '''
+        View function that gets all events being advertised
+        '''
+        return render(request, 'all/index.html')
