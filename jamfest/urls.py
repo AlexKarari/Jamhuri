1d4ba66ae67b0c28599e0f3b3ea2f82c9bd5d291
alexkarari <k.alexander@gmail.com>
2018-05-31 15:13:40 +0300

    Artists
    All artists to be booked are now displayed

--- jamfest/urls.py
+++ jamfest/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     url('^$', views.index, name='landingpage'),
     url(r'^news/(\d+)/$', views.news, name='news'),
+    url(r'^all_artists/$', views.all_artists, name='all_artists'),
+    url(r'^solo_artist/(\d+)/$', views.single_artist, name='solo_artist'),
     
 ]
 if settings.DEBUG:
--- jamfest/views.py
+++ jamfest/views.py
@@ -17,9 +17,26 @@ def index(request):
     return render(request, 'all/index.html', {"events": events, "news": news, "testimonials": testimonials, "service": service, "merchs": merchs})
 
 
+def all_artists(request):
+    '''
+    View function that displays all artists in the company's agency
+    '''
+    artists = Artist.objects.all()
+    return render(request, 'all/allartists.html', {"artists": artists})
+
+
+def single_artist(request, artist_id):
+    try:
+        artists = Artist.objects.get(id=artist_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, "all/single_artist.html", {"artists": artists})
+
 def news(request, news_id):
     try:
         news = News.objects.get(id=news_id)
     except DoesNotExist:
         raise Http404()
     return render(request, "all/news.html", {"news": news})
+
+
