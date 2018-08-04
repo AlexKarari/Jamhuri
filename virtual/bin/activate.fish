3b3c1fd188545586190827a561c7c95eb9a968bc
alexkarari <k.alexander@gmail.com>
2018-08-05 01:18:16 +0300

    Virtual folder
    new virtual folder created. All dependancies installed

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alexander/Documents/Jamhuri/virtual"
+set -gx VIRTUAL_ENV "/home/alexander/Documents/jamhuri/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
